bed4c6a3e19fde411f2fd421ecda1d8b5a1d658f
Harsh Gaur <hgaur491@gmail.com>
2025-05-22 13:11:12 +0530

    eslint fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
-import Navbar from "@/components/ui/navbar";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -29,10 +28,7 @@ export default function RootLayout({
         <html lang="en" suppressHydrationWarning>
             <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
                 <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
-                    {/* <div className="flex flex-col min-h-screen relative bg-gradient-to-b from-white to-neutral-50 dark:from-neutral-950 dark:to-black transition-colors duration-300"> */}
-                        {/* <Navbar /> */}
-                        {children}
-                        {/* </div> */}
+                    {children}
                 </ThemeProvider>
             </body>
         </html>
