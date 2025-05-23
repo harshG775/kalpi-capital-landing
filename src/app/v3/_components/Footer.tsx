a857da157049367d0317b2cbf78f8a1619203ea3
Harsh Gaur <hgaur491@gmail.com>
2025-05-23 16:40:27 +0530

    feat: implement Footer component with logo and navigation links

--- src/app/v3/_components/Footer.tsx
+++ src/app/v3/_components/Footer.tsx
@@ -1,3 +1,74 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Footer() {
-    return <div>Footer</div>;
+    const footerLink = {
+        logo: {
+            image: "/kalpi-logo.jpeg",
+            link: "/",
+        },
+        links: [
+            {
+                title: "Home",
+                link: "/",
+            },
+            {
+                title: "Pricing",
+                link: "/",
+            },
+            {
+                title: "About",
+                link: "/",
+            },
+            {
+                title: "Blog",
+                link: "/",
+            },
+            {
+                title: "Contact",
+                link: "/",
+            },
+        ],
+    };
+    return (
+        <footer>
+            <div className="flex max-w-7xl mx-auto gap-4 py-16 px-4 flex-col flex-wrap md:flex-row md:items-center">
+                <Link href={footerLink.logo.link} className="w-[80px] flex justify-start md:justify-center">
+                    <Image
+                        src={footerLink.logo.image}
+                        alt="logo"
+                        width={80}
+                        height={80}
+                        className="mix-blend-multiply h-12 w-auto object-contain"
+                    />
+                </Link>
+
+                <div className="flex md:items-center items-start flex-1 gap-4 md:flex-row flex-col ">
+                        <Link href="/" className="text-muted-foreground hover:text-foreground transition-colors font-semibold">
+                            Get Started for Free
+                        </Link>
+                    {footerLink.links.map((link, index) => (
+                        <Link
+                            key={index}
+                            href={link.link}
+                            className="text-muted-foreground hover:text-foreground transition-colors"
+                        >
+                            {link.title}
+                        </Link>
+                    ))}
+
+                    <Link
+                        href="/"
+                        className="md:ml-auto text-muted-foreground hover:text-foreground transition-colors"
+                    >
+                        Terms
+                    </Link>
+                    <Link href="/" className="text-muted-foreground hover:text-foreground transition-colors">
+                        Privacy
+                    </Link>
+                </div>
+            </div>
+        </footer>
+    );
 }
